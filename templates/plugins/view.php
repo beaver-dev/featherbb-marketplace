1f22e5484d23cf843fda532c4104bc9dd4914579
beaver-dev <bladerunner.vs@gmail.com>
2016-02-10 18:01:26 +0100

    New plugins infos display

--- templates/plugins/view.php
+++ templates/plugins/view.php
@@ -1,35 +1,23 @@
+<?php
+$tag_links = [];
+foreach (explode(',',$plugin->keywords) as $tag) {
+    $tag_links[] = '<a href="'.Router::pathFor('plugins.tags', ['tag' => str_replace([' ', '.'], '-', $tag)]).'">#'.$tag.'</a>';
+}
+?>
 <div class="plugin-head">
-    <h3><?= $plugin->name; ?></h3>
-    <small><?= $plugin->nb_downloads; ?> downloads</small>
-    <div class="row">
-        <div class="six columns">
-            <em><?= isset($plugin->description) ? $plugin->description : 'No description available'; ?></em>
-        </div>
-        <div class="six columns">
-            <a href="<?= Router::pathFor('plugins.download', ['name'=>$plugin->vendor_name]); ?>" class="button u-pull-right">Download</a>
-        </div>
-    </div>
-    <ul id="plugin-nav" class="inline">
+    <h2><a href="#"><?= $plugin->name; ?></a> <small><?= isset($plugin->last_version) ? $plugin->last_version : '0.1.0'; ?></small></h2>
+    <em><?= isset($plugin->description) ? $plugin->description : 'No description available'; ?></em>
+    <p><?= $plugin->nb_downloads; ?> downloads</p>
+    <p><?= implode(', ', $tag_links); ?></p>
+</div>
+
+<div id="plugin-nav">
+    <a class="button" href="<?= Router::pathFor('plugins.download', ['name'=>$plugin->vendor_name]); ?>">Download</a>
 <?php foreach($plugin->menus as $menu): ?>
-        <li>
-            <a href="<?= Router::pathFor('plugins.view', ['name'=>$plugin->vendor_name, 'action'=>strtolower($menu)]); ?>"><?= $menu; ?></a>
-        </li>
+    <a class="button" href="<?= Router::pathFor('plugins.view', ['name'=>$plugin->vendor_name, 'action'=>strtolower($menu)]); ?>"><?= $menu; ?></a>
 <?php endforeach; ?>
-        <li>
-            <a href="<?= Router::pathFor('plugins.view', ['name'=>$plugin->vendor_name, 'action'=>'history']); ?>">History</a>
-        </li>
-    </ul>
 </div>
 
 <div class="plugin-body">
     <?= $content; ?>
-</div>
-<?php
-$tag_links = [];
-foreach (explode(',',$plugin->keywords) as $tag) {
-    $tag_links[] = '<a href="'.Router::pathFor('plugins.tags', ['tag' => str_replace([' ', '.'], '-', $tag)]).'">'.$tag.'</a>';
-}
-?>
-<div class="plugin-footer">
-    Tags: <?= implode(', ', $tag_links); ?>
 </div>
