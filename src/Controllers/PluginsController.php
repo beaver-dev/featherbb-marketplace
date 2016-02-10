c6a184cf801911e1160059c9bbee47c2d90f764f
adaur <adaur.underground@gmail.com>
2016-02-10 15:30:45 +0100

    Get rid of Skeleton, add more checks

--- src/Controllers/PluginsController.php
+++ src/Controllers/PluginsController.php
@@ -54,7 +54,8 @@ class PluginsController {
         $plugin_id = Request::getParsedBody()['plugin_id'];
 
         if (PluginModel::downloadData($vendor_name) === false) {
-            echo "error archive"; return;
+            $notFoundHandler = Container::get('notFoundHandler');
+            return $notFoundHandler(Container::get('request'), Container::get('response'));
         }
         // If no errors, store generic infos to DB
         $plugin = PluginModel::accept($plugin_id, $vendor_name);
@@ -63,6 +64,12 @@ class PluginsController {
     public function view($req, $res, $args)
     {
         $plugin = PluginModel::getData($args['name']);
+
+        if ($plugin === false) {
+            $notFoundHandler = Container::get('notFoundHandler');
+            return $notFoundHandler(Container::get('request'), Container::get('response'));
+        }
+
         $action = isset($args['action']) ? $args['action'] : 'description';
 
         if ($action === 'history') {
@@ -83,9 +90,12 @@ class PluginsController {
     public function download($req, $res, $args)
     {
         $plugin = ORM::for_table('plugins')->where('vendor_name', $args['name'])->find_one();
+
         if (!$plugin) {
-            return "not found";
+            $notFoundHandler = Container::get('notFoundHandler');
+            return $notFoundHandler(Container::get('request'), Container::get('response'));
         }
+
         $plugin->nb_downloads = $plugin->nb_downloads+1;
         $plugin->save();
         $version = isset($args['version']) ? $args['version'] : 'master';
@@ -97,6 +107,12 @@ class PluginsController {
     public function history($req, $res, $args)
     {
         $plugin = PluginModel::getData($args['name']);
+
+        if ($plugin === false) {
+            $notFoundHandler = Container::get('notFoundHandler');
+            return $notFoundHandler(Container::get('request'), Container::get('response'));
+        }
+
         $history = PluginModel::history($args['name']);
 
         return View::setPageInfo(['plugin' => $plugin, 'markdown' => $markdown])
--- src/Models/Plugin.php
+++ src/Models/Plugin.php
@@ -88,17 +88,23 @@ class Plugin
 
     public static function accept($plugin_id, $vendor_name)
     {
-        $composer = file_get_contents(getcwd()."/pluginsdata/$vendor_name/composer.json");
-        $description = json_decode($composer)->description;
-        $keywords = implode(',', json_decode($composer)->keywords);
+        $plugin = false;
 
-        $plugin = ORM::for_table('plugins')->find_one($plugin_id);
-        $plugin->homepage = 'https://github.com/featherbb/'.$vendor_name;
-        $plugin->status = 2;
-        $plugin->vendor_name = $vendor_name;
-        $plugin->description = $description;
-        $plugin->keywords = $keywords;
-        $plugin->save();
+        if (file_exists(getcwd()."/pluginsdata/$vendor_name/composer.json")) {
+            $composer = file_get_contents(getcwd()."/pluginsdata/$vendor_name/composer.json");
+            $description = json_decode($composer)->description;
+            $keywords = implode(',', json_decode($composer)->keywords);
+
+            $plugin = ORM::for_table('plugins')->find_one($plugin_id);
+            if ($plugin !== false) {
+                $plugin->homepage = 'https://github.com/featherbb/'.$vendor_name;
+                $plugin->status = 2;
+                $plugin->vendor_name = $vendor_name;
+                $plugin->description = $description;
+                $plugin->keywords = $keywords;
+                $plugin->save();
+            }
+        }
 
         return $plugin;
     }
@@ -107,32 +113,32 @@ class Plugin
     {
         $plugin = ORM::for_table('plugins')->where('vendor_name', $vendor_name)->find_one();
 
-        if ($plugin === false) {
-            App::notFound();
+        if (file_exists(getcwd()."/pluginsdata/$vendor_name/composer.json") && $plugin !== false) {
+            $composer = file_get_contents(getcwd()."/pluginsdata/$vendor_name/composer.json");
+            $plugin->composer = json_decode($composer);
+            // $plugin->readme = file_get_contents(getcwd()."/pluginsdata/$vendor_name/README.md");
+
+            // Get menu items to display in plugin infos...
+            preg_match_all('/\s\s#{2} (.+)/S', $plugin->readme, $plugin_menus, PREG_PATTERN_ORDER);
+            $plugin->menus = $plugin_menus[1];
+            // var_dump($plugin_menus[1]);
+
+            // Parse readme to get each h2 body content...
+            $results = preg_split('/\s\s## \w+\s\s.*?/', $plugin->readme, -1, PREG_SPLIT_NO_EMPTY);
+            $plugin->general_infos = $results[0];
+            array_shift($results);
+            // var_dump($results);
+
+            // And associate h2 menu items with their content as an array
+            $menu_content = [];
+            foreach ($results as $key => $result) {
+                $menu_key = strtolower($plugin->menus[$key]);
+                $menu_content[$menu_key] = $result;
+            }
+            $plugin->menu_content = $menu_content;
+        } else {
+            $plugin = false;
         }
-
-        $composer = file_get_contents(getcwd()."/pluginsdata/$vendor_name/composer.json");
-        $plugin->composer = json_decode($composer);
-        // $plugin->readme = file_get_contents(getcwd()."/pluginsdata/$vendor_name/README.md");
-
-        // Get menu items to display in plugin infos...
-        preg_match_all('/\s\s#{2} (.+)/S', $plugin->readme, $plugin_menus, PREG_PATTERN_ORDER);
-        $plugin->menus = $plugin_menus[1];
-        // var_dump($plugin_menus[1]);
-
-        // Parse readme to get each h2 body content...
-        $results = preg_split('/\s\s## \w+\s\s.*?/', $plugin->readme, -1, PREG_SPLIT_NO_EMPTY);
-        $plugin->general_infos = $results[0];
-        array_shift($results);
-        // var_dump($results);
-
-        // And associate h2 menu items with their content as an array
-        $menu_content = [];
-        foreach ($results as $key => $result) {
-            $menu_key = strtolower($plugin->menus[$key]);
-            $menu_content[$menu_key] = $result;
-        }
-        $plugin->menu_content = $menu_content;
 
         return $plugin;
     }
